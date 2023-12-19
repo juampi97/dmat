f9fca98a2e9beb675392719f9865d0ba30ea6af6
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-12-19 14:32:08 -0300

    Mis datos

---
+++ js/cuenta/load_datos.js
@@ -0,0 +1,74 @@
+import { getCookie } from "../functions.js";
+
+window.addEventListener('load',() => {
+    const datos = document.querySelector("#datos");
+
+    let nombre = getCookie('nombre')
+    let email = getCookie('email')
+    let empresa = getCookie('empresa')
+    let cuit = getCookie('cuit')
+
+    if(window.innerWidth > 500){
+    datos.innerHTML = `
+            <div class="row">
+                <div class="col-2">
+                    <p><strong>Nombre</strong></p>
+                </div>
+                <div class="col-6">
+                    <p>${nombre}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <p><strong>Email</strong></p>
+                </div>
+                <div class="col-6">
+                    <p>${email}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <p><strong>Empresa</strong></p>
+                </div>
+                <div class="col-6">
+                    <p>${empresa}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <p><strong>CUIT</strong></p>
+                </div>
+                <div class="col-6">
+                    <p>${cuit}</p>
+                </div>
+            </div>
+            `
+    } else {
+        datos.innerHTML = `
+            <div class="row">
+                <div class="flex-row">
+                    <p><strong>Nombre</strong></p>
+                    <p>${nombre}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="">
+                    <p><strong>Email</strong></p>
+                    <p>${email}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="">
+                    <p><strong>Empresa</strong></p>
+                    <p>${empresa || 'No definido'}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="">
+                    <p><strong>CUIT</strong></p>
+                    <p>${cuit || 'No definido'}</p>
+                </div>
+            </div>
+            `
+    }
+})
