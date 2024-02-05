ecff4614f4141059d927fedffba7426a51feddbe
Juan Pablo Calabro <juampicalabro97@gmail.com>
2024-02-05 10:34:53 -0300

    mail

---
+++ js/contacto.js
@@ -0,0 +1,84 @@
+let productionMailRoute = "https://mail-dmat.onrender.com/mail";
+let devMailRoute = "http://localhost:8080/mail";
+let enviroment = "prod"; // prod || dev
+let mailRoute = "";
+
+switch (enviroment) {
+    case "dev":
+      mailRoute = devMailRoute;
+      break;
+    case "prod":
+      mailRoute = productionMailRoute;
+      break;
+  }
+
+  window.addEventListener("load", () => {
+    const buttonSend = document.querySelector("#buttonSend");
+    const contactForm = document.querySelector("#contactForm");
+  
+    contactForm.addEventListener("submit", runVerify);
+  });
+
+  // Funciones generar mail
+
+const runVerify = (e) => {
+    e.preventDefault();
+    runCaptcha();
+  };
+  
+  const runCaptcha = () => {
+    grecaptcha.ready(function () {
+      grecaptcha
+        .execute("6Lf6lzApAAAAALlQ1HOfO6HwtwpnbR3YP7kRdge_", { action: "submit" })
+        .then(function (token) {
+          // Add your logic to submit to your backend server here.
+          const captcha = token;
+          getData(captcha);
+          postData(captcha);
+        });
+    });
+  };
+  
+  const getData = (captcha) => {
+    let datos = new FormData(contactForm);
+    let datosProcesados = Object.fromEntries(datos.entries());
+    datosProcesados.captcha = captcha
+    return datosProcesados;
+  };
+  
+  const postData = async (captcha) => {
+    const newMessage = getData(captcha);
+    try {
+      const response = await fetch(mailRoute, {
+        method: "POST",
+        headers: { 
+          "Accept": "application/json, text-/plain, */*",
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(newMessage),
+      }).then((res) => res.json()).then((data)=>{
+        Swal.fire({
+          text: `Mensaje: ${data.mensaje}`,
+          icon: "success"
+        });
+        contactForm.reset()
+        // location.reload()
+        Swal.fire({
+          text: `Mensaje: ${data.mensaje}`,
+          icon: "success",
+        });
+        setTimeout(() => {
+          let carritoVacio = [];
+          localStorage.setItem("carrito", JSON.stringify(carritoVacio));
+          location.reload();
+        }, 5000);
+      })  
+  
+    } catch (error) {
+      Swal.fire({
+        text: `Mensaje: ${data.mensaje}`,
+        icon: "error"
+      });
+    }
+  };
+  
